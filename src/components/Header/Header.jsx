daf945ae10244d123c6e87285dd3909f2e7b9c42
AlfredoP977 <A_parodi97@hotmail.com>
2025-04-09 11:31:37 -0400

    added add button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import "./Header.css";
 import defaultAvatar from "../../assets/defaultAvatar.svg";
 import weatherLogo from "../../assets/weatherLogo.svg";
-function Header() {
+
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img src={weatherLogo} alt="" className="header__logo" />
       <p className="header__date-and-location">DATE, LOCATION</p>
 
       <div className="header__user-container">
-        <button className="header__add-clothes-btn">+ Add clothes</button>
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add clothes
+        </button>
         <p className="header__username">Terrence Tegegne</p>
         <img
           src={defaultAvatar}
